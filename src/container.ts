211ae2a5383c000ceb9c86e4e742d60671ec9219
marig012 <mariana.gonzaleza@upb.edu.co>
2026-05-19 14:32:06 -0500

    registrar ReservationRepository en container

--- src/container.ts
+++ src/container.ts
@@ -6,6 +6,7 @@ import { RouteStopRepository } from './repositories/RouteStopRepository.js';
 import { SeedRepository } from './repositories/SeedRepository.js';
 import { UserRepository } from './repositories/UserRepository.js';
 import { getEnv } from './config/env.js';
+import { ReservationRepository } from './repositories/ReservationRepository.js';
 
 export type Repositories = {
   users: UserRepository;
@@ -14,6 +15,7 @@ export type Repositories = {
   routes: RouteRepository;
   routeStops: RouteStopRepository;
   seed: SeedRepository;
+  reservations: ReservationRepository;
 };
 
 let repos: Repositories | null = null;
@@ -29,6 +31,7 @@ export function getRepositories(): Repositories {
       routes: new RouteRepository(pool),
       routeStops: new RouteStopRepository(pool),
       seed: new SeedRepository(pool, SEED_CSV_DIR),
+      reservations: new ReservationRepository(pool),
     };
   }
   return repos;
